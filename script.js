058f0b8478947a94145f3f67a25f282d810ad775
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-02 09:48:39 +0800

    updated the calculation of the score to be based on clicks

--- script.js
+++ script.js
@@ -5,34 +5,36 @@ var score = 0;
 // total score
 var totalScore = 0;
 
+var clicks = 0;
+
 //placeholder name, until I make an input for dynamic
 var name = "Mau";
 
 
 //make array with the photos
- var gameArray = [
-        case1 = {answer: "bears", url: "images/bears.jpg"},
-        case3 = {answer: "cat", url: "images/cat.jpg"},
-        case4 = {answer: "dog", url: "images/dog.jpg"},
-        case5 = {answer: "plane", url: "images/plane.jpg"},
-        case6 = {answer: "racoon", url: "images/racoon.jpg"},
-        case7 = {answer: "robot", url: "images/robot.jpg"}
-    ];
+var gameArray = [
+    case1 = { answer: "bears", url: "images/bears.jpg" },
+    case3 = { answer: "cat", url: "images/cat.jpg" },
+    case4 = { answer: "dog", url: "images/dog.jpg" },
+    case5 = { answer: "plane", url: "images/plane.jpg" },
+    case6 = { answer: "racoon", url: "images/racoon.jpg" },
+    case7 = { answer: "robot", url: "images/robot.jpg" }
+];
 
 // console.log(gameArray);
 // console.log(gameArray[1].answer);
 //covering buttons grid
 var grid = [
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null, null]
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null]
 
 ]
 
@@ -52,48 +54,48 @@ var header = document.createElement('header');
 body.appendChild(header);
 //title with settimeout of 1 sec //omited the timeout part
 var heading = document.createElement('h1');
-    header.appendChild(heading);
-    heading.innerText = "What's underneath";
+header.appendChild(heading);
+heading.innerText = "What's underneath";
 
 //section for buttons
 var navigationDiv = document.createElement('div');
-    body.appendChild(navigationDiv);
-    navigationDiv.classList.add('nav-div');
+body.appendChild(navigationDiv);
+navigationDiv.classList.add('nav-div');
 
 //the instructions button
 var instructionsBtn = document.createElement('button');
-    navigationDiv.appendChild(instructionsBtn);
-    instructionsBtn.setAttribute('id', 'instructions-btn');
-    instructionsBtn.innerText = "Instructions";
-    instructionsBtn.addEventListener('click', expandInstructions);
+navigationDiv.appendChild(instructionsBtn);
+instructionsBtn.setAttribute('id', 'instructions-btn');
+instructionsBtn.innerText = "Instructions";
+instructionsBtn.addEventListener('click', expandInstructions);
 
 //by default hidden, section with the instruction text
 // var instructionsDiv = document.createElement('div');
 function expandInstructions(event) {
 
     var instructionsDiv = document.createElement('div');
-        instructionsDiv.classList.add('show-instructions');
-        instructionsDiv.setAttribute('id', 'instructions');
-        body.appendChild(instructionsDiv);
+    instructionsDiv.classList.add('show-instructions');
+    instructionsDiv.setAttribute('id', 'instructions');
+    body.appendChild(instructionsDiv);
 
 
     var instructionsText = document.createElement('p');
-        instructionsDiv.appendChild(instructionsText);
-        instructionsText.textContent = "Guess what's underneath and describe the illustration with one word to win. Click on the squares to reveal any part of the hidden image. Type your answer in the input field and submit. Less squares you reveal before guessing, higher the score."
+    instructionsDiv.appendChild(instructionsText);
+    instructionsText.textContent = "Guess what's underneath and describe the illustration with one word to win. Click on the squares to reveal any part of the hidden image. Type your answer in the input field and submit. Less squares you reveal before guessing, higher the score."
 
-         // header.style.display = 'none';
-        navigationDiv.style.display = 'none';
+    // header.style.display = 'none';
+    navigationDiv.style.display = 'none';
 
 
-        body.style.backgroundColor = "#333333";
-        body.style.color = "white";
+    body.style.backgroundColor = "#333333";
+    body.style.color = "white";
 
-        var exitBtn = document.createElement('button');
-        instructionsDiv.appendChild(exitBtn);
-        exitBtn.setAttribute('id','exit-btn');
-        exitBtn.innerText = "Exit";
-        // exitBtn.addEventListener('click', reset(expandInstructions));
-        exitBtn.addEventListener('click', exit);
+    var exitBtn = document.createElement('button');
+    instructionsDiv.appendChild(exitBtn);
+    exitBtn.setAttribute('id', 'exit-btn');
+    exitBtn.innerText = "Exit";
+    // exitBtn.addEventListener('click', reset(expandInstructions));
+    exitBtn.addEventListener('click', exit);
 
 }
 
@@ -103,7 +105,7 @@ function exit() {
     body.style.backgroundColor = "white";
     body.style.color = "black";
     // instructionsDiv.style.display = 'none';
-     document.getElementById('instructions').style.display = "none";
+    document.getElementById('instructions').style.display = "none";
     // instructionsDiv.reset();
     exitBtn.style.display = "none";
 }
@@ -111,10 +113,10 @@ function exit() {
 
 //the start button
 var start = document.createElement('button');
-    navigationDiv.appendChild(start);
-    start.setAttribute('id', 'start');
-    start.innerText = "Start";
-    start.addEventListener('click', startGame);
+navigationDiv.appendChild(start);
+start.setAttribute('id', 'start');
+start.innerText = "Start";
+start.addEventListener('click', startGame);
 
 //game screen --------->
 
@@ -126,70 +128,75 @@ function startGame(event) {
 
     // create the main appended to the body
     var main = document.createElement('main');
-        body.appendChild(main);
-        console.log('start clicked');
-        start.style.display = 'none';
-        header.style.display = 'none';
-        navigationDiv.style.display = "none";
-        body.style.backgroundColor = "#333333";
+    body.appendChild(main);
+    console.log('start clicked');
+    start.style.display = 'none';
+    header.style.display = 'none';
+    navigationDiv.style.display = "none";
+    body.style.backgroundColor = "#333333";
 
     //create a div for the image underneath
     var img = document.createElement('img');
-        main.appendChild(img);
-        img.setAttribute('id', 'current-img');
-        img.setAttribute('src', 'images/bears.jpg');
+    main.appendChild(img);
+    img.setAttribute('id', 'current-img');
+    img.setAttribute('src', 'images/bears.jpg');
 
 
     var hideImgDiv = document.createElement('div');
-        main.appendChild(hideImgDiv);
-        hideImgDiv.classList.add('hide-img');
+    main.appendChild(hideImgDiv);
+    hideImgDiv.classList.add('hide-img');
 
-//create grid of buttons to hide the image
-         for (var i = 0; i < grid.length; i++) {
+    //create grid of buttons to hide the image
+    for (var i = 0; i < grid.length; i++) {
 
-                var row = document.createElement('div');
-                row.classList.add('row')
-                hideImgDiv.appendChild(row);
+        var row = document.createElement('div');
+        row.classList.add('row')
+        hideImgDiv.appendChild(row);
 
-                for (var j = 0 ; j < grid[i].length; j++) {
-                    // console.log(grid[i][j]);
-               // var row = document.querySelector('.row')
-                    var button = document.createElement('button');
-                    button.setAttribute('id', `button${j+1}`)
-                    button.classList.add('square')
+        for (var j = 0; j < grid[i].length; j++) {
+            // console.log(grid[i][j]);
+            // var row = document.querySelector('.row')
+            var button = document.createElement('button');
+            button.setAttribute('id', `button${j+1}`)
+            button.classList.add('square')
 
-                    row.appendChild(button);
-                //add event listener, on click to become invisible
-                    button.addEventListener('click', function() {
-                    this.style.visibility = "hidden";
-                    })
-                }
-       };
+            row.appendChild(button);
+            //add event listener, on click to become invisible
+            button.addEventListener('click', function() {
+                this.style.visibility = "hidden";
+                clicks ++;
+                document.getElementById('clicks').innerHTML = `Squares opened: ${clicks}`;
+                // score = score - (score * 0.20);
+            })
+        }
+    };
 
-   setTimeout(inputBoard, 800);
+    setTimeout(inputBoard, 800);
     setTimeout(scoreBoard, 1200);
 
 }
 
- //call this func from scoreboard func with 2 sec timeout
-    var newImg = function() {
-        var square = document.querySelectorAll('.square');
-            for (var i=0; i < square.length; i++) {
-                square[i].style.visibility = "visible";
-            }
-            var img = document.getElementById('current-img');
-        img.setAttribute('src', `${gameArray[counter].url}`);
+//call this func from scoreboard func with 2 sec timeout
+var newImg = function() {
+    var square = document.querySelectorAll('.square');
+    for (var i = 0; i < square.length; i++) {
+        square[i].style.visibility = "visible";
+    }
+    var img = document.getElementById('current-img');
+    img.setAttribute('src', `${gameArray[counter].url}`);
 
 
     var text = document.getElementById('checkText');
     console.log('beforeeeeeeeeeeeee', text.innerText);
     text.innerText = "";
-     document.getElementById('new-score').innerHTML = `New Score: 0`;
+    document.getElementById('new-score').innerHTML = `New Score: 0`;
+    document.getElementById('clicks').innerHTML = `Squares opened: 0`;
     console.log('zfterrrrrrrr', text.innerText)
     document.getElementById('output').innerText = "";
+    document.getElementById('correct-answer').innerText = "";
     counter++;
-        // img.setAttribute('src', `${gameArray[counter].url}`);
-    }
+    // img.setAttribute('src', `${gameArray[counter].url}`);
+}
 
 
 
@@ -204,40 +211,46 @@ var inputBoard = function() {
 
 
     var answerBoard = document.createElement('aside');
-        body.appendChild(answerBoard);
-        answerBoard.setAttribute('id', 'answer-board');
-        // answerBoard.insertBefore(main, body);
+    body.appendChild(answerBoard);
+    answerBoard.setAttribute('id', 'answer-board');
+    // answerBoard.insertBefore(main, body);
 
 
     //input question
     var question = document.createElement('h2');
-        answerBoard.appendChild(question);
-        question.innerHTML = "Guess what's <br/>hidden underneath?";
+    answerBoard.appendChild(question);
+    question.innerHTML = "Guess what's <br/>hidden underneath?";
 
 
 
 
     //input
     var inputField = document.createElement('input');
-        answerBoard.appendChild(inputField);
-        inputField.setAttribute('id','input-field');
-        inputField.setAttribute('placeholder','Type here...')
+    answerBoard.appendChild(inputField);
+    inputField.setAttribute('id', 'input-field');
+    inputField.setAttribute('placeholder', 'Type here...')
 
-     var showAnswer = document.createElement('h3');
-        answerBoard.appendChild(showAnswer);
-        showAnswer.setAttribute('id','output');
+    var showAnswer = document.createElement('h3');
+    answerBoard.appendChild(showAnswer);
+    showAnswer.setAttribute('id', 'output');
 
 
-  var displayCorrectOrFalse = document.createElement('h3');
-            displayCorrectOrFalse.setAttribute('id','checkText');
-            answerBoard.appendChild(displayCorrectOrFalse);
-            displayCorrectOrFalse.innerText = "";
+    var displayCorrectOrFalse = document.createElement('h3');
+    displayCorrectOrFalse.setAttribute('id', 'checkText');
+    answerBoard.appendChild(displayCorrectOrFalse);
+    displayCorrectOrFalse.innerText = "";
 
-        inputField.addEventListener('change', function(event){
+    var displayCorrectAnswer = document.createElement('h3');
+    displayCorrectAnswer.setAttribute('id', 'correct-answer');
+    answerBoard.appendChild(displayCorrectAnswer);
+    displayCorrectAnswer.innerText = "";
+
+
+    inputField.addEventListener('change', function(event) {
         var currentInput = event.target.value;
         event.target.value = "";
         var square = document.querySelectorAll('.square');
-            for (var i=0; i < square.length; i++) {
+            for (var i = 0; i < square.length; i++) {
                 square[i].style.visibility = "hidden";
 
             }
@@ -247,7 +260,7 @@ var inputBoard = function() {
 
         var output = inputHappened(currentInput);
         checkAnswer(output);
-        display( output );
+        display(output);
 
         // counter++;
         setTimeout(newImg, 2000);
@@ -260,105 +273,118 @@ var inputBoard = function() {
 
         // console.log(showAnswer.innerText);
         // console.log(showAnswer);
-      })
+    })
 
 
-        var display = function( data ){
+    var display = function(data) {
         var output = document.querySelector('#output');
         output.innerText = data;
-      }
+    }
 
 
-      var checkAnswer = function(userAnswer) {
+    var checkAnswer = function(userAnswer) {
 
 
-            var currentImg = document.getElementById('current-img');
-            var currentImgUrl = currentImg.getAttribute('src');
-            console.log(currentImgUrl);
+        var currentImg = document.getElementById('current-img');
+        var currentImgUrl = currentImg.getAttribute('src');
+        console.log(currentImgUrl);
 
 
         // for (var i = 0; i < gameArray.length; i++) {
-            var gameAnswer = gameArray[counter-1].answer;
+        var gameAnswer = gameArray[counter - 1].answer;
 
-            // var arrayImg = gameArray[i].url;
-            // console.log(arrayImg);
-            console.log(userAnswer);
-            console.log(gameAnswer);
-            if(userAnswer === gameAnswer) {
-                // console.log('true');
-                displayCorrectOrFalse.innerText = "Correct";
-                displayCorrectOrFalse.style.color = "#E3CD81";
+        // var arrayImg = gameArray[i].url;
+        // console.log(arrayImg);
+        console.log(userAnswer);
+        console.log(gameAnswer);
+        if (userAnswer === gameAnswer) {
+            // console.log('true');
+            displayCorrectOrFalse.innerText = "Correct";
+            displayCorrectOrFalse.style.color = "#E3CD81";
 
-                score = 5000;
+            score = 5000;
 
-                totalScore += score;
+            score = score - (clicks * 0.2);
 
-
-
-                document.getElementById('total-score').innerHTML = `Total Score: ${totalScore}`;
-                 document.getElementById('new-score').innerHTML = `New Score: ${score}`;
-                // updateScore.innerHTML = "dgsgd";
-                console.log('in correct func!')
-
-                // update()
-
-            } else {
-
-                 displayCorrectOrFalse.innerText = "False";
-                displayCorrectOrFalse.style.color = "#D8514E";
-                console.log('in wronf func!')
-
-            }
-
-
-      }
-
-
-    };
+            totalScore += score;
 
 
 
+            document.getElementById('total-score').innerHTML = `Total Score: ${totalScore}`;
+            document.getElementById('new-score').innerHTML = `New Score: ${score}`;
+            // document.getElementById('clicks').innerHTML = `Clicks: ${clicks}`;
+            // updateScore.innerHTML = "dgsgd";
+            console.log('in correct func!')
+
+            // update()
+
+        } else {
+
+            displayCorrectOrFalse.innerText = "False";
+
+            displayCorrectAnswer.innerText = `${gameAnswer} is the correct answer`;
+            displayCorrectOrFalse.style.color = "#D8514E";
+            console.log('in wronf func!')
+            console.log("this is correct" + gameAnswer);
+
+        }
 
 
-var inputHappened = function(currentInput){
+    }
 
-// console.log(output.innerText);
-// if(output.innerText === "bears") {
-//     console.log('mau');
-// } else {
-//     console.log('av');
-// };
-
-
-  return currentInput;
 
 };
 
 
-function scoreBoard()  {
 
-     var scoreAside = document.createElement('aside');
-        body.appendChild(scoreAside);
-        scoreAside.setAttribute('id', 'score-aside');
+
+
+var inputHappened = function(currentInput) {
+
+    // console.log(output.innerText);
+    // if(output.innerText === "bears") {
+    //     console.log('mau');
+    // } else {
+    //     console.log('av');
+    // };
+
+
+    return currentInput;
+
+};
+
+
+function scoreBoard() {
+
+    var scoreAside = document.createElement('aside');
+    body.appendChild(scoreAside);
+    scoreAside.setAttribute('id', 'score-aside');
 
     //name placeholder
     var playerName = document.createElement('h2');
-        scoreAside.appendChild(playerName);
-        // playerName.innerHTML = "Name";
-        playerName.innerHTML = `Name: ${name}`;
+    scoreAside.appendChild(playerName);
+    // playerName.innerHTML = "Name";
+    playerName.innerHTML = `Name: ${name}`;
 
-     //total score placeholder
+    //total score placeholder
     var totalScoreElement = document.createElement('h2');
-        scoreAside.appendChild(totalScoreElement);
-         totalScoreElement.setAttribute('id', 'total-score');
-        totalScoreElement.innerHTML = `Total Score: ${totalScore}`;
+    scoreAside.appendChild(totalScoreElement);
+    totalScoreElement.setAttribute('id', 'total-score');
+    totalScoreElement.innerHTML = `Total Score: ${totalScore}`;
 
-     //new score placeholder
+    //new score placeholder
     var newScoreElement = document.createElement('h2');
-        scoreAside.appendChild(newScoreElement);
+    scoreAside.appendChild(newScoreElement);
 
-        newScoreElement.innerHTML = `New Score:${score}`;
-         newScoreElement.setAttribute('id', 'new-score');
+    newScoreElement.innerHTML = `New Score:${score}`;
+    newScoreElement.setAttribute('id', 'new-score');
+
+    //new clicks placeholder
+    var clicksElement = document.createElement('h2');
+    scoreAside.appendChild(clicksElement);
+
+    clicksElement.innerHTML = `Squares opened: ${clicks}`;
+    clicksElement.setAttribute('id', 'clicks');
 
 }
 
